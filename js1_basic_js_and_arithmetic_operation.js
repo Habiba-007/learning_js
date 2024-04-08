5de7a3e8196a1455cb4a3ee1131ea2ebd364b7a0
jueab1 <hoque273@gmail.com>
2024-04-09 02:18:44 +0600

    arithmetic operator

---
+++ js1_basic_js_and_arithmetic_operation.js
@@ -0,0 +1,41 @@
+// console.log("Hello HungryCoder")
+// console.log('ok,paise');
+// console.log('ok paise thank you');
+
+// var first_number;
+// let second_number;
+// const third_number = 30;
+// fourth_number = 400;
+
+
+// first_number = "eta r change hobe na"
+
+// var first_number
+// first_number = "amar number"
+
+// let farid;
+// farid = 'good shot';
+// console.log(farid);
+
+// let jueab;
+// jueab = 100;
+// console.log(jueab);
+
+let a, b, sum, sub_result, multi_result, devide_result, modulus_result;
+a = 10;
+b = 25;
+sum = a + b;
+sub_result = b - a;
+multi_result = b * a;
+devide_result = b / a;
+modulus_result = b % a;
+// console.log(sum);
+// console.log(sub_result);
+// console.log(multi_result);
+// console.log(devide_result);
+// console.log(modulus_result);
+console.log('sum_result->' + sum);
+console.log('sub:' + sub_result);
+console.log('multi:' + multi_result);
+console.log('dev:' + devide_result);
+console.log('mod:'+ modulus_result);
